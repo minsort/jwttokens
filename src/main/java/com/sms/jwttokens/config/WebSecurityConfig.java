49a9efda0fce4e353b7f50f536366ad580813764
minsort <minsort@yandex.ru>
2022-05-11 15:11:18 +0300

    debug package jwt

--- src/main/java/com/sms/jwttokens/config/WebSecurityConfig.java
+++ src/main/java/com/sms/jwttokens/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.annotation.Resource;
 
